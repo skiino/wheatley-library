7f3f8cf75feeec7fb0027e27aa4ddcfae652f634
Sarah Kiino <sarah.kiino@gmail.com>
2017-03-30 08:39:43 -0400

    tweaked schedule page so only teacher cells highlighted on hover

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@
 /*HIGHLIGHT TABLE CELLS ON HOVER--SCHEDULE PAGE*/
 
 
-$("td").hover(function(){
+$(".teacher").hover(function(){
     $(this).css("background-color", "#ffa1a1");
     }, function(){
     $(this).css("background-color", "#fff");
